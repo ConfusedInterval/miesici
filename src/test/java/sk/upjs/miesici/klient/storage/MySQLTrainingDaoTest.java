a433eebaaae9fc2b32495add5c49c58f04e840b3
Windows <ConfusedInterval>
2019-12-17 23:10:18 +0100

    Unit test, bug fixed,..

---
+++ src/test/java/sk/upjs/miesici/klient/storage/MySQLTrainingDaoTest.java
@@ -0,0 +1,57 @@
+package sk.upjs.miesici.klient.storage;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sk.upjs.miesici.admin.storage.Customer;
+import sk.upjs.miesici.admin.storage.CustomerDao;
+import sk.upjs.miesici.admin.storage.DaoFactory;
+
+import java.sql.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MySQLTrainingDaoTest {
+    TrainingDao dao = DaoFactory.INSTANCE.getTrainingDao();
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAll() {
+        List<Training> all = dao.getAll();
+        assertNotNull(all);
+        assertTrue(all.size() > 0);
+    }
+
+    @Test
+    void saveTraining() {
+        List<Training> all = dao.getAll();
+        Training training = new Training();
+        training.setClientId(1L);
+        training.setDate(Date.valueOf("2020-01-01"));
+        training.setName("good");
+        training.setNote("one");
+        training.setDayofTheWeek(3);
+        dao.saveTraining(training);
+        assertEquals(all.size() + 1, dao.getAll().size());
+    }
+
+    @Test
+    void getAllbyClientId() {
+        assertEquals(1, dao.getAllbyClientId(5L).size());
+    }
+
+    @Test
+    void deleteTrainingById() {
+        List<Training> all = dao.getAll();
+        dao.deleteTrainingById(6L);
+        assertEquals(all.size() - 1, dao.getAll().size());
+    }
+}
